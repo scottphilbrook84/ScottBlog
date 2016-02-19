3b11bfa8c5e8e585b3332b5be28fea378b1d2dd3
sphilbrook84 <ernieballsep@gmail.com>
2016-02-18 23:02:47 -0500

    Corrections

--- src/Models/Blog.cs
+++ src/Models/Blog.cs
@@ -1,9 +1,9 @@
 
 namespace ScottBlog.Models
 {
-    public class Blog
-    {
-        public int BlogId { get; set; }
+    public class BlogId
+ {
+        public int Id { get; set; }
         public string Url { get; set; }   
     }        
 }
