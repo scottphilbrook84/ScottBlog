8896ef10172caa2c60999053538fbb4f33f0276e
Scott Philbrook <sphilbrook@CCENSUS.COM>
2016-03-21 22:55:47 -0400

    Cleaning up

---
+++ test/ScottBlog.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace ScottBlog.Tests
+{
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void ReturnFalseGivenValuesLessThan2(int value)
+    {
+        var result = _primeService.IsPrime(value);
+
+        Assert.False(result, String.Format("{0} should not be prime", value));
+    }
+}
